785e2b7743fbe41f6c5b5232dae3076022552905
Kaden Franklin <kadenfranklin@hotmail.com>
2023-02-14 19:48:19 -0600

    renamed wall script

--- Lab 2/Assets/Scripts/Wall.cs
+++ Lab 2/Assets/Scripts/Wall.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grave : MonoBehaviour
+public class Wall : MonoBehaviour
 {
 
     public string text;
